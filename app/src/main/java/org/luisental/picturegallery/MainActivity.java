3e98a8f30cd67fff9b11a0d5f20be00f348bd947
Diemo <diemo@luisental.org>
2019-09-23 20:31:57 +0200

    Main Activity with Button; View Activity.

--- app/src/main/java/org/luisental/picturegallery/MainActivity.java
+++ app/src/main/java/org/luisental/picturegallery/MainActivity.java
@@ -1,8 +1,10 @@
 package org.luisental.picturegallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void viewGallery(View view) {
+        Intent intent = new Intent(this,  ViewGallery.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/org/luisental/picturegallery/ViewGallery.java
@@ -0,0 +1,16 @@
+package org.luisental.picturegallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ViewGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_gallery);
+    }
+
+}
